88d05241006a641fae1c44bbd7879c897cd0e8cc
dengjj <18993882884@163.com>
2024-12-19 16:40:00 +0800

    feat: 06

--- 06-Cameras/06-cameras/src/script.js
+++ 06-Cameras/06-cameras/src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 /**
  * Base
  */
@@ -18,17 +18,29 @@ const scene = new THREE.Scene()
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.MeshBasicMaterial({ color: 0x8acefe })
 )
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 scene.add(camera)
+
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+// camera.position.x = 2
+// camera.position.y = 2
+// camera.position.z = 2
+
+// camera.lookAt(mesh.position)
+
+// 
+const axesHelper = new THREE.AxesHelper(4)
+scene.add(axesHelper)
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -36,14 +48,28 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
+
+// Cursor
+// const cursor = {
+//     x: 0,
+//     y: 0
+// }
+
+// window.addEventListener('mousemove', (event) => {
+//     cursor.x = event.clientX / sizes.width - 0.5
+//     cursor.y = - (event.clientY / sizes.height - 0.5)
+// })
+
+
+const controls = new OrbitControls(camera, canvas)
+// controls.target.y = 1;
+// controls.update()
+controls.enableDamping = true;
+
 // Animate
-const clock = new THREE.Clock()
-
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
